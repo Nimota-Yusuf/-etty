c61c1b0f2ca7518b7b314f135cc8537bba2ff65e
Nimota-Yusuf <nimota2001@gmail.com>
2023-04-22 14:42:54 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing an output using printf
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n);
+	Return (0);
+}
