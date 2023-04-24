3f928ad17e3f5e74ef585155cc3c26fb0454fc50
Nimota-Yusuf <nimota2001@gmail.com>
2023-04-23 17:45:50 -0700

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("Size of char: %lu bytes(s)\n", (unsigned long)sizeof(a));
-printf("Size of int: %lu bytes(s)\n", (unsigned long)sizeof(b));
-printf("Size of long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of char:%lu bytes(s)\n", (unsigned long)sizeof(a));
+printf("Size of int:%lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("Size of long int:%lu bytes(s)\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("Size of float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+printf("Size of float:%lu bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
